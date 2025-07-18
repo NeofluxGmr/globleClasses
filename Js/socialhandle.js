bc25c030bbbd12884588388e0e00c3229ac66bea
NeofluxGmr <neofluxgmr@gmail.com>
2025-07-18 10:54:04 +0530

    Add files via upload

---
+++ Js/socialhandle.js
@@ -0,0 +1,17 @@
+      
+//   function for social media handle   
+
+function hreflink(no){
+    if (no == 1){
+        location.href = "https://www.facebook.com/share/1DqfG8wyaS/"
+    }
+    else if(no == 2){
+        location.href = "https://www.instagram.com/global_online_classes"
+    }
+    else if(no == 3){
+        location.href = "https://www.youtube.com/@globalonlineclasses"
+    }
+    else{
+        console.log("Invalid Link")
+    }
+}
