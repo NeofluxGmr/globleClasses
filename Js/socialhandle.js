067397e6eacb8318b8e5ede256a5163c505d106c
NeofluxGmr <neofluxgmr@gmail.com>
2025-07-19 12:39:58 +0530

    Update socialhandle.js

--- Js/socialhandle.js
+++ Js/socialhandle.js
@@ -1,25 +1,46 @@
-const Enterypoint = document.getElementById("Enterypoint");
+   const Enterypoint = document.getElementById("Enterypoint");
+    const slider = document.getElementById("slider");
+    const slides = slider.querySelectorAll("img");
+    const dotsContainer = document.getElementById("dots");
+    let currentSlide = 0;
 
-
-
-function hreflink(no){
-    if (no == 1){
-        location.href = "https://www.facebook.com/share/1DqfG8wyaS/"
+    function updateDots() {
+      dotsContainer.innerHTML = "";
+      slides.forEach((_, i) => {
+        const dot = document.createElement("div");
+        dot.className = "dot-btn" + (i === currentSlide ? " active" : "");
+        dot.onclick = () => {
+          currentSlide = i;
+          slider.scrollTo({ left: slider.clientWidth * currentSlide, behavior: "smooth" });
+          updateDots();
+        };
+        dotsContainer.appendChild(dot);
+      });
     }
-    else if(no == 2){
-        location.href = "https://www.instagram.com/global_online_classes"
-    }
-    else if(no == 3){
-        location.href = "https://www.youtube.com/@globalonlineclasses"
-    }
-    else{
-        console.log("Invalid Link")
-    }
-}
 
+    function autoSlide() {
+      currentSlide = (currentSlide + 1) % slides.length;
+      slider.scrollTo({ left: slider.clientWidth * currentSlide, behavior: "smooth" });
+      updateDots();
+    }
 
-window.onload = function () {
-  setTimeout(function () {
-    Enterypoint.style.display = "none";
-    }, 2000); 
-  };
+    setInterval(autoSlide, 3000); // auto-slide every 4 seconds
+
+    window.onload = () => {
+      updateDots();
+      setTimeout(() => {
+        Enterypoint.style.opacity = "0";
+        setTimeout(() => {
+          Enterypoint.style.display = "none";
+        }, 500);
+      }, 2500);
+    };
+
+    function hreflink(no) {
+      const links = [
+        "https://www.facebook.com/share/1DqfG8wyaS/",
+        "https://www.instagram.com/global_online_classes",
+        "https://www.youtube.com/@globalonlineclasses"
+      ];
+      if (links[no - 1]) location.href = links[no - 1];
+    }
