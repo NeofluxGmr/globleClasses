cf031f77bb09117b31fb313fedda0aeee44d1833
NeofluxGmr <neofluxgmr@gmail.com>
2025-07-23 17:59:53 +0530

    Create firebase.js

---
+++ Js/firebase.js
@@ -0,0 +1,16 @@
+// firebase.js (Node backend example)
+const firebase = require("firebase/app");
+require("firebase/firestore");
+require("firebase/auth");
+
+firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+});
+
+module.exports = firebase;
